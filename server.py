b4d536bc33a629e0bb0d0d06cf70a0b9ff5edd96
Pavlo Malynin <pavlo@malynin.ca>
2019-01-24 15:13:12 -0700

    Assn

--- server.py
+++ server.py
@@ -1,46 +1,153 @@
 #  coding: utf-8 
-import socketserver
+import asyncio
+from urllib.parse import urlparse
+import pathlib
+import os
+import stat
 
-# Copyright 2013 Abram Hindle, Eddie Antonio Santos
-# 
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-# 
-#     http://www.apache.org/licenses/LICENSE-2.0
-# 
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-#
-# Furthermore it is derived from the Python documentation examples thus
-# some of the code is Copyright © 2001-2013 Python Software
-# Foundation; All Rights Reserved
-#
-# http://docs.python.org/2/library/socketserver.html
-#
-# run: python freetests.py
+www_folder = pathlib.Path("./www").resolve()
 
-# try: curl -v -X GET http://127.0.0.1:8080/
+class HttpException(Exception):
+    def __init__(self, code, reason):
+        super().__init__(reason)
+        self.code = code
+        self.reason = reason
 
+    def write_error(self, writer):
+        writer.write(get_status_line(self.code, self.reason))
+        write_header(writer, ("Connection", "close"))
 
-class MyWebServer(socketserver.BaseRequestHandler):
+class MethodNotAllowed(HttpException):
+    def __init__(self):
+        super().__init__(405, "Method Not Allowed")
+
+    def write_error(self, writer):
+        super().write_error(writer)
+        write_header(writer, ("Allow", "GET"))
+
+class PermanentRedirect(HttpException):
+    def __init__(self, location):
+        super().__init__(301, "Moved Permanently")
+        self.location = location
+
+    def write_error(self, writer):
+        super().write_error(writer)
+        write_header(writer, ("Location", self.location))
+
+class HttpEnd(Exception):
+    pass
+
+async def parse_http(reader):
+    try:
+        request_line = await reader.readline()
+    except ConnectionResetError:
+        raise HttpEnd()
+    request_line = request_line.decode("utf-8")
+    if not request_line:
+        raise HttpEnd()
+
+    request_line = request_line.strip()
+    method, uri, version = request_line.split(" ")
+
+    if method != "GET":
+        raise MethodNotAllowed()
     
-    def handle(self):
-        self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+    if version != "HTTP/1.1":
+        raise HttpException(400, "Bad Request")
+    
+    headers = {}
 
-if __name__ == "__main__":
-    HOST, PORT = "localhost", 8080
+    while True:
+        line = await reader.readline()
+        line = line.decode("utf-8").strip().lower()
+        if not line:
+            break
+        header, value = line.split(":", maxsplit=1)
+        if header in headers:
+            headers[header] = [headers[header], value]
+        else:
+            headers[header] = value
 
-    socketserver.TCPServer.allow_reuse_address = True
-    # Create the server, binding to localhost on port 8080
-    server = socketserver.TCPServer((HOST, PORT), MyWebServer)
+    if "host" in headers:
+        uri = urlparse(f"//{headers['host']}{uri}").path
+    else:
+        uri = urlparse(uri).path
 
-    # Activate the server; this will keep running until you
-    # interrupt the program with Ctrl-C
-    server.serve_forever()
+    return (uri, headers)
+
+def write_header(writer, header):
+    writer.write(f"{header[0]}: {str(header[1])}\r\n".encode("utf-8"))
+
+async def send_file(writer, stats, filepath):
+    try:
+        filetype = filepath.suffix
+        if filetype == ".html":
+            filetype = "text/html; charset=utf-8"
+        elif filetype == ".css":
+            filetype = "text/css; charset=utf-8"
+        else:
+            filetype = "application/octet-stream"
+
+        writer.write(get_status_line(200, "OK"))
+        write_header(writer, ("Content-Type", filetype))
+        write_header(writer, ("Content-Length", stats.st_size))
+        writer.write(b"\r\n")
+        with open(filepath, "rb") as fd:
+            await asyncio.get_running_loop().sendfile(writer.transport, fd)
+        await writer.drain()
+    except:
+        raise HttpEnd()
+
+async def dispatch_request(writer, request):
+    filepath = pathlib.Path(www_folder, f".{request[0]}")
+    try:
+        # Security check
+        filepath.resolve().relative_to(www_folder)
+
+        stats = os.stat(filepath)
+
+        if stat.S_ISDIR(stats.st_mode):
+            if request[0][-1] != "/":
+                raise PermanentRedirect(request[0] + "/")
+            filepath = pathlib.Path(filepath, "index.html")
+            stats = os.stat(filepath)
+            await send_file(writer, stats, filepath)
+        else:
+            # try to send directly
+            await send_file(writer, stats, filepath)
+    except (FileNotFoundError,ValueError):
+        raise HttpException(404, "Not Found")
+    
+    
+    
+        
+
+def get_status_line(code, message):    
+    sline = ["HTTP/1.1", str(code), message, "\r\n"]
+    return " ".join(sline).encode("utf-8")
+
+async def error_response(writer, err: HttpException):
+    err.write_error(writer)
+    writer.write(b"\r\n")
+
+    await writer.drain()
+    writer.close()
+
+
+async def handle(reader, writer):
+    try:
+        while True:
+            request = await parse_http(reader)
+            await dispatch_request(writer, request)
+    except HttpException as err:
+        await error_response(writer, err)
+    except HttpEnd:
+        pass
+
+async def main():
+    server = await asyncio.start_server(handle, host="127.0.0.1", port=8080)
+    async with server:
+        await server.serve_forever()
+
+
+asyncio.run(main())
